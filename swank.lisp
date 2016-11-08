e0734558a716702669ed8b9873ef22f28be15856
caio <caioaao@gmail.com>
2016-11-08 00:34:43 -0200

    Added messages to swank server commands

--- swank.lisp
+++ swank.lisp
@@ -7,13 +7,17 @@
 (require :swank)
 (swank-loader:init)
 
+(defvar server-port 4004)
+
 (defun start-swank ()
-  (swank:create-server :port 4004
+  (swank:create-server :port server-port
                        :style swank:*communication-style*
-                       :dont-close t))
+                       :dont-close t)
+  (message "Server created at port ~s" server-port))
 
 (defun stop-swank ()
-  (swank:stop-server 4004))
+  (swank:stop-server server-port)
+  (message "Stopped server at port ~s" server-port))
 
 (defcommand swank-start () () (start-swank))
 (defcommand swank-stop  () () (stop-swank))
